b9c7c115cf6d79474634c9b6767eac3039dfae61
GasGas7 <79295810+GasGas7@users.noreply.github.com>
2021-06-15 13:49:14 +0200

    @Header : add title and buttons

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,5 +2,5 @@ let mix = require('laravel-mix');
 
 mix
     .js('src/app.js', 'js/main.js')
-    .sass('src/app.scss', 'css/style.css')
+    .sass('src/app.scss', 'css/style.css').options({ processCssUrls: false })
     .setPublicPath('dist');
